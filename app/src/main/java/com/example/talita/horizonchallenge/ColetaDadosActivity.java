6ffd5b4c7c4907f721c85b0446c605dc13ba9e24
TalitaRamos <talitasantosrd@gmail.com>
2018-08-04 13:36:24 -0300

    Verificar de campos foram preenchidos

--- app/src/main/java/com/example/talita/horizonchallenge/ColetaDadosActivity.java
+++ app/src/main/java/com/example/talita/horizonchallenge/ColetaDadosActivity.java
@@ -3,20 +3,27 @@ package com.example.talita.horizonchallenge;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
+import android.media.effect.Effect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class ColetaDadosActivity extends AppCompatActivity  implements TextView.OnClickListener{
+    private EditText nome;
+    private EditText cpf;
     private TextView dataView;
     private Button buttonColeta;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton4;
     static final int DATE_DIALOG_ID = 0;
 
     @Override
@@ -24,15 +31,21 @@ public class ColetaDadosActivity extends AppCompatActivity  implements TextView.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coleta_dados);
 
+        nome = (EditText)findViewById(R.id.nome);
+        cpf = (EditText)findViewById(R.id.cpf);
         dataView=(TextView)findViewById(R.id.dataView);
         buttonColeta=(Button) findViewById(R.id.buttonColeta);
+        radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
+        radioButton4=(RadioButton)findViewById(R.id.radioButton4);
 
         dataView.setOnClickListener(this);
 
         buttonColeta.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+            if(validarCampos()!=false){
                 Intent it = new Intent(ColetaDadosActivity.this, VisualizarDadosActivity.class);
                 startActivity(it);
+            }
             }
         });
     }
@@ -71,4 +84,34 @@ public class ColetaDadosActivity extends AppCompatActivity  implements TextView.
             showDialog(DATE_DIALOG_ID);
     }
     //**** FIM SELECIONADOR DE DATA
+
+    //VERIFICANDO SE OS CAMPOS FORAM PREENCHIDOS
+    private boolean validarCampos(){
+        boolean resultado=true;
+
+        if (nome.getText().toString().length() == 0) {
+            resultado=false;
+            nome.setError("Digite seu nome!");
+        }
+        if(dataView.getText().toString().equals("dd/mm/aaaa")){
+            resultado=false;
+            dataView.setError("Insira uma data!");
+        }else{
+            dataView.setError(null);
+        }
+        if(cpf.getText().toString().length() == 0){
+            resultado=false;
+            cpf.setError("Digite seu cpf!");
+        }
+
+        int selectedItemID =radioGroup.getCheckedRadioButtonId();
+        if(selectedItemID<=0){
+            resultado=false;
+            radioButton4.setError("Selecione uma opção!");
+        }else{
+            radioButton4.setError(null);
+        }
+
+        return resultado;
+    }
 }
